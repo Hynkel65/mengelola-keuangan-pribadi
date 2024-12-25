f46c26e6fca7c4b99a6a977871572d5fca828bfe
Hynkel65 <117414669+Hynkel65@users.noreply.github.com>
2024-12-25 14:33:19 +0700

    Ready for first deploy

--- config/db.js
+++ config/db.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     mongoose.set('strictQuery', true);
+
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
     } catch (err) {
+        console.log(`Error: ${err.message}`.red);
         process.exit(1);
     }
 }
